acb92360907bf47dd4c63d96aab74d6b1b63302a
s2312611 <171270978+s2312611@users.noreply.github.com>
2026-01-12 02:46:24 +0900

    20260112 | Added /Sources/ShiftJIS/preserve.bash

---
+++ Sources/ShiftJIS/preserve.bash
@@ -0,0 +1,11 @@
+find . -type f -name "*.shp" | while
+    read -r shapefile
+do
+    name="${shapefile%.shp}"
+    codepagefile="$name.cpg"
+    if
+        [ ! -e "$codepagefile" ]
+    then
+        printf "SJIS\n" > "$codepagefile"
+    fi
+done
