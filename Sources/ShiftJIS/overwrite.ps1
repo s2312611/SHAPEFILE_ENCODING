8f1f051621b1eb23b174746d41e8b5889478f830
s2312611 <171270978+s2312611@users.noreply.github.com>
2026-01-12 02:53:27 +0900

    20260112 | Added /Sources/ShiftJIS/overwrite.ps1

---
+++ Sources/ShiftJIS/overwrite.ps1
@@ -0,0 +1,6 @@
+Get-ChildItem -Recurse -Filter *.shp | ForEach-Object {
+    $ShapeFile = $_
+    $Name = Join-Path -Path $ShapeFile.DirectoryName -ChildPath $ShapeFile.BaseName
+    $CodePageFile = "$Name.cpg"
+    Set-Content -Path $CodePageFile -Value "SJIS`n"
+}
