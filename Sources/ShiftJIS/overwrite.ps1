99f05b455d3074aa2cd8e7a5888c9a7da3621172
s2312611 <171270978+s2312611@users.noreply.github.com>
2026-01-28 18:03:10 +0900

    20260128 | Modified /Sources/ShiftJIS/overwrite.ps1

--- Sources/ShiftJIS/overwrite.ps1
+++ Sources/ShiftJIS/overwrite.ps1
@@ -1,3 +1,4 @@
+#!/usr/bin/env pwsh
 Get-ChildItem -Recurse -Filter *.shp | ForEach-Object {
     $Shapefile = $_
     $Name = Join-Path -Path $Shapefile.DirectoryName -ChildPath $Shapefile.BaseName
