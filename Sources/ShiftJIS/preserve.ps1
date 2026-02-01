404dc3332a660cfe6fb2362163e6c6b645c6b235
s2312611 <171270978+s2312611@users.noreply.github.com>
2026-02-02 02:11:14 +0900

    20260202 | Modified /Sources/ShiftJIS/preserve.ps1

--- Sources/ShiftJIS/preserve.ps1
+++ Sources/ShiftJIS/preserve.ps1
@@ -1,11 +1,9 @@
 #!/usr/bin/env pwsh
 Get-ChildItem -Recurse -Filter *.shp | ForEach-Object {
-    $Shapefile = $_
-    $Name = Join-Path -Path $Shapefile.DirectoryName -ChildPath $Shapefile.BaseName
-    $CodePageFile = "$Name.cpg"
+    $CodePageFile="$(Join-Path -Path $_.DirectoryName -ChildPath $_.BaseName).cpg"
     if (
-        -not (Test-Path -Path $CodePageFile)
-    ){
-        Set-Content -Path $CodePageFile -Value "SJIS`n"
+        Test-Path -Path $CodePageFile
+    ){} else {
+        Set-Content -Path "$CodePageFile" -Value "SJIS"
     }
 }
